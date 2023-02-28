87f30b944d26c101ed76892eb00cf98bb2b6d71f
wangffan1 <wangffan@mail.ustc.edu.cn>
2023-02-28 16:27:21 +0800

    first commit

---
+++ wangff_web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HeadTab />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import HeadTab from '@/components/head_tab'
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      HeadTab,
+    }
+  }
+</script>
+
+
