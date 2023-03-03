8354e23450d6b4dd9b5436634b9fd7348815ce45
weiw8 <weiw8@cisco.com>
2023-03-03 23:05:27 +0800

    add router

--- wangff_web/src/router/index.js
+++ wangff_web/src/router/index.js
@@ -10,6 +10,39 @@ const routes = [
         path: '/',
         name: 'layout',
         component: Layout,
+        children: [{
+            path: '/',
+            name: '/Epidemic_distribution',
+            redirect: '/Epidemic_distribution',
+            component: () => 
+                import('../views/main.vue'),       
+            children: [
+                {
+                path: '/',
+                name: '/Epidemic_distribution',
+                component: () => 
+                    import('../views/Epidemic_distribution/index.vue')
+                },
+                {
+                path: '/',
+                name: '/Provinces',
+                component: () => 
+                    import('../views/Provinces/index.vue')
+                },
+                {
+                path: '/',
+                name: '/Vaccine_administration',
+                component: () => 
+                    import('../views/Vaccine_administration/index.vue')
+                },
+                {
+                path: '/',
+                name: '/National_policies',
+                component: () => 
+                    import('../views/National_policies/index.vue')
+                }
+            ] 
+        }]
     }
 ]
 
